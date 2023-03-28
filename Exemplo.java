a07e81b5664bd78decce339e2f5ec8e53cbbc5cd
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-03-28 11:16:02 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe um valor inteiro: ");
+		int valor1 = sc.nextInt();
+		
+		
+		System.out.println("Informe um valor inteiro: ");
+		int valor2 = sc.nextInt();
+		
+		
+	
+		int soma  = valor1 + valor2;
+        System.out.println("O resultado é: " + soma);
+        System.out.println("O resultado da soma he:" + valor1 + valor2 );
+        System.out.println(valor1 + valor2 + " Oi!!");
+        System.out.println("O resultado da soma he :" + (valor1 + valor2));
+        
+        
+		
+		
+		
+		
+
+	}
+
+}
