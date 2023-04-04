4b3aa59d8a4421d89570166aaeb623c02bc20b57
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-04 11:35:36 -0300

    Add files via upload

---
+++ Quiosque.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Quiosque {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Qtd de bolas");
+		int qtdBolas = sc.nextInt();
+		
+		if (sabor.equalsIgnoreCase("C")) {
+			if (qtdBolas >= 3) {
+				System.out.println("Desconto de 10%");
+			} else {
+				System.out.println("Desconto de 5%");
+		}
+	} else if (sabor.equalsIgnoreCase("M")) {
+		System.out.println("Sem Desconto");
+	} else {
+		System.out.println("Sabor Invalido");
+
+	} {
+
+		}
+		
+	
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+
+	}
+
+}
