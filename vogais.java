4667b50d4c416aee86cd659b1e5d55e88856c3eb
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-04 15:44:02 -0300

    Update and rename Ex.java to vogais.java

---
+++ vogais.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe uma Vogal ");
+		// String vogal = sc.next();
+		// String vogal = sc.next().toUpperCase();
+		char vogal = sc.next().charAt(0);
+		
+		// if(vogal.equals("A")){
+		if(vogal == 'A')}
+		System.out.println("é vogal")
+	}else {
+	System.out.println("não vogal")
+		}
+		
+	
+		
+		
+		sc.close();
+	}
+
+}
