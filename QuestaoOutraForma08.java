699a0d9c8702f63422738e0e0c3541e626ce78c3
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-04 11:08:53 -0300

    Add files via upload

---
+++ QuestaoOutraForma08.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class QuestaoOutraForma08 {
+
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um valor");
+		int valor1 = sc.nextInt();
+		
+		if ((valor1 > 100) || (valor1 <200)) {
+			System.out.println("Está no Intervalo");						
+		} else {
+
+		}
+		
+		
+		
+
+	}
+
+}
