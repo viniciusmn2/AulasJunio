1cf925e311c21278f09b8b10ccaa5ab6676622ad
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-10 11:07:04 -0300

    Add files via upload

---
+++ Tabuada7.java
@@ -0,0 +1,16 @@
+
+public class Tabuada7 {
+	        // Elabore um programa,que mostre a tabuada de 
+			//multiplicaçao de 7
+	public static void main(String[] args) {
+		
+		int valor = 1;
+		
+		while(valor <= 10) {
+		System.out.println(" 7 * " + valor + " : " + (7 * valor));	
+		valor ++;
+		}
+
+	}
+
+}
