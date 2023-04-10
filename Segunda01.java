e197c45d2c1a47feb6477f8be2597e1ae03e6480
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-10 10:58:16 -0300

    Add files via upload

---
+++ Segunda01.java
@@ -0,0 +1,13 @@
+
+public class Segunda01 {
+
+	public static void main(String[] args) {
+		
+		String resultado = (numero % 2 == 0) ? "PAR" : "Impar";
+		
+		System.out.println(resultado);
+		entrada.close();
+
+	}
+
+}
