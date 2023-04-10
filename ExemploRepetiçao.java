8e89a9a5ff964f832ad834dcd2449eb7582b7910
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-10 10:58:31 -0300

    Add files via upload

---
+++ ExemploRepetiçao.java
@@ -0,0 +1,15 @@
+
+public class ExemploRepetiçao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		
+		while (x <= 20) {
+			System.out.println(x + " : DEUS");
+			x++;
+		}
+
+	}
+
+}
