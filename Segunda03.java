77e3de7957adbff9c93094548898fed3a24594cd
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-10 10:57:56 -0300

    Add files via upload

---
+++ Segunda03.java
@@ -0,0 +1,19 @@
+
+public class Segunda03 {
+
+	public static void main(String[] args) {
+		
+		String nome  = "Deus";
+		int idade    = 2023;
+		double x     = 5.6;
+		
+		System.out.printf("Seu nome é %s %n", nome);
+		System.out.printf("Seu nome é %d %n", idade);
+		System.out.printf("Seu nome é %.2f", x);
+		
+		System.out.printf("Nome é  %s, idade %d e ptoFlu %f", nome,idade,x);
+		
+
+	}
+
+}
