abbeb6607f807619588e60cefa0ad1b922ea126d
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-04-18 11:35:43 -0300

    Add files via upload

---
+++ Questao06.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Questao06 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		int[] A = new int [5];
+		int[] B = new int [5];
+		
+		for (int i=0; i<=5; i++) {
+			System.out.println("Informe uma nota :");
+			A[i]= entrada.nextInt();
+			B[i]= entrada.nextInt();
+			
+		}
+		
+		System.out.println("\n Valores do Vetor A:")
+  		
+				
+				
+
+	}
+
+}
