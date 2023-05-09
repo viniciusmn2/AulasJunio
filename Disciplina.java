71948c82127c24796588caf41303fd894fe939f4
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-05-09 20:42:04 -0300

    Add files via upload

--- Disciplina.java
+++ Disciplina.java
@@ -1,4 +1,40 @@
+package POO.DisciplinaV2;
+	public class Disciplina {
+	
 
-public class Disciplina {
+		
+	private double nota;
+	private String disciplina;
 
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Disciplina [nota=" + nota + ", disciplina=" + disciplina + "]";
+	}
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
