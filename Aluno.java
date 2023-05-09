ff48780b2c84ba2b292327c4eceb7651541a1b93
viniciusmn2 <128382464+viniciusmn2@users.noreply.github.com>
2023-05-09 20:42:33 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,4 +1,80 @@
+package POO.DisciplinaV2;
 
-public class Aluno {
+import java.util.ArrayList;
+import java.util.List;
+
+import POO.Disciplina.Disciplina;
+
+
+	public class Aluno {
+
+		private String nome;
+		private int idade;
+		private String faculdade;
+
+		List<Disciplina> disciplina = new ArrayList<Disciplina>();
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+		public String getFaculdade() {
+			return faculdade;
+		}
+
+		public void setFaculdade(String faculdade) {
+			this.faculdade = faculdade;
+		}
+
+		public List<Disciplina> getDisciplina() {
+			return disciplina;
+		}
+
+		public void setDisciplina(List<Disciplina> disciplina) {
+			this.disciplina = disciplina;
+		}
+
+		@Override
+		public String toString() {
+			return "Aluno: " + nome + "Média: " + media() + "Situação:" + situacaoAluno() + "]";
+		}
+
+		public double media() {
+
+			double someNotas = 0.0;
+			for (Disciplina disciplina : disciplina) {
+				someNotas += disciplina.getNota();
+
+			}
+			return someNotas / disciplina.size();
+		}
+
+		public String situacaoAluno() {
+
+			String situacao;
+			if (media() < 70) {
+
+				situacao = "aprovado";
+
+			} else {
+
+				situacao = "Reprovado";
+			}
+			return situacao;
+		}
+
+	}
 
 }
